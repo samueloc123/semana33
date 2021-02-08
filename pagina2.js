cd6cf41e0d798f76ba43adee749f31a0cca0b8d5
samueloc123 <72421709+samueloc123@users.noreply.github.com>
2021-02-07 21:59:38 -0500

    Add files via upload

---
+++ pagina2.js
@@ -0,0 +1,18 @@
+var  iuno = function ( ) {
+	var  n1 = documento . getElementById ( "h1" ) ;
+	var  n2 = documento . getElementById ( "im3" ) ;
+
+var sn1=document.getElementById("sn1");
+
+sn1.autoplay=true;
+sn1.load();
+
+if ("sn1"=="sn1")
+{
+setTimeout(stop2,1000);
+}
+
+	n1 . className = "h1d" ;
+		n2 . className = "im3d" ;
+
+}
